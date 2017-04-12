a498a89058c26c55a087731be9860f45b96ef2ef
ilyasprogrammer <rakhimkulov@it-projects.info>
2017-04-12 18:22:35 +0500

    [IMP] own checker name. + using wrapper for checks

--- pylint_odoo/checkers/itp_checks.py
+++ pylint_odoo/checkers/itp_checks.py
@@ -1,3 +1,4 @@
+import io
 import re
 import ast
 import os
@@ -5,24 +6,44 @@ import types
 import astroid
 from pylint.checkers import BaseChecker, utils
 from pylint.interfaces import IAstroidChecker
-from .. import settings
+from .. import misc, settings
 
-ITP_CHECK_PREFIX_ID = 77
 ITP_ODOO_MSGS = {
     # C->convention R->refactor W->warning E->error F->fatal
-    'E%d01' % ITP_CHECK_PREFIX_ID: (
+    'E%d99' % settings.BASE_OMODULE_ID: (
         'Necessary manifest field "%s" got wrong value "%s"',
-        'skipped-field',
+        'manifest-template-field',
+        settings.DESC_DFLT
+    ),
+    'E%d98' % settings.BASE_OMODULE_ID: (
+        'File doc/index.rst is absent in module',
+        'absent-doc',
+        settings.DESC_DFLT
+    ),
+    'E%d97' % settings.BASE_OMODULE_ID: (
+        'File doc/changelog.rst is absent in module',
+        'absent-changelog',
+        settings.DESC_DFLT
+    ),
+    'E%d96' % settings.BASE_OMODULE_ID: (
+        'File: %s - Template field "%s" is not filled',
+        'rst-template-field',
         settings.DESC_DFLT
     ),
 }
+TEMPLATE_RE = '{[_ a-zA-Z0-9]*}'
+TEMPLATE_FILES = ('README.rst', 'doc/index.rst', 'doc/changelog.rst')
 
 
-class ITPModuleChecker(BaseChecker):
+class ITPModuleChecker(misc.WrapperModuleChecker):
     __implements__ = IAstroidChecker
 
-    name = settings.CFG_SECTION
+    name = 'itplynt'
     msgs = ITP_ODOO_MSGS
+
+    @utils.check_messages(*(ITP_ODOO_MSGS.keys()))
+    def visit_module(self, node):
+        self.wrapper_visit_module(node)
 
     def visit_dict(self, node):
         if not os.path.basename(self.linter.current_file) in settings.MANIFEST_FILES \
@@ -33,6 +54,47 @@ class ITPModuleChecker(BaseChecker):
         # Check all template fields filled
         for k, v in manifest_dict.items():
             if isinstance(v, types.StringTypes):
-                match = re.match('{[_ a-zA-Z0-9]*}', v)
+                match = re.match(TEMPLATE_RE, v)
                 if match:
-                    self.add_message('skipped-field', node=node, args=(k, v))
+                    self.add_message('manifest-template-field', node=node, args=(k, v))
+
+    def open(self):
+        """Define variables to use cache"""
+        self.inh_dup = {}
+        super(ITPModuleChecker, self).open()
+
+    def close(self):
+        """Final process get all cached values and add messages"""
+        for (odoo_node, class_dup_name), nodes in self.inh_dup.items():
+            if len(nodes) == 1:
+                continue
+            path_nodes = []
+            for node in nodes[1:]:
+                relpath = os.path.relpath(node.file,
+                                          os.path.dirname(odoo_node.file))
+                path_nodes.append("%s:%d" % (relpath, node.lineno))
+            self.add_message('consider-merging-classes-inherited',
+                             node=nodes[0],
+                             args=(class_dup_name, ', '.join(path_nodes)))
+
+    def _check_absent_doc(self):
+        return os.path.isfile(os.path.join(self.module_path, 'doc/index.rst'))
+
+    def _check_absent_changelog(self):
+        return os.path.isfile(os.path.join(self.module_path, 'doc/changelog.rst'))
+
+    def _check_rst_template_field(self):
+        rst_files = self.filter_files_ext('rst')
+        self.msg_args = []
+        for rst_file in rst_files:
+            if rst_file in TEMPLATE_FILES:
+                f = io.open(os.path.join(self.module_path, rst_file))
+                content = f.read()
+                f.close()
+                match = re.findall(TEMPLATE_RE, content)
+                if len(match):
+                    for rec in match:
+                        self.msg_args.append(("%s" % rst_file, rec))
+        if self.msg_args:
+            return False
+        return True
